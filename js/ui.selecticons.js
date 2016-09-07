831f1a12d7a5e37704aecb3041becc992aa0cecf
Vlad Moyseenko <vlad.dancer@gmail.com>
2016-09-08 02:24:13 +0300

    Issues #2769987, #2770053 by vlad.dancer: respect an element ajax, add permanent scrollbar

--- js/ui.selecticons.js
+++ js/ui.selecticons.js
@@ -1,4 +1,7 @@
-(function($) {
+(function ($, Drupal, drupalSettings, window) {
+
+  'use strict';
+
   $.widget('custom.selecticons', $.ui.selectmenu, {
     _renderItem: function( ul, item ) {
       var li = $( "<li>", { text: item.label } );
@@ -17,25 +20,49 @@
     }
   });
 
-  $('.selecticons').selecticons({
-    create: function( event, ui ) {
-      // Get classes of currently selected option.
-      var currentClasses = $(this).find('option:selected').attr('data-class');
-      var button = $('#' + this.id + '-button');
+  Drupal.behaviors.select_icons = {
+    attach: function (context) {
 
-      // Create element for current selection's icon.
-      button.prepend('<span class="ui-current-item-icon ' + currentClasses + '"></span>')
-    },
-    select: function( event, ui ) {
-      // Get current option's icon element.
-      var currentIcon = $('#' + this.id + '-button > .ui-current-item-icon');
+      $('.selecticons').selecticons({
+        create: function (event, ui) {
+          // Get classes of currently selected option.
+          var currentClasses = $(this).find('option:selected').attr('data-class');
+          var button = $('#' + this.id + '-button');
 
-      // Get selected option's classes.
-      var classes = ui.item.element.attr('data-class');
+          // Create element for current selection's icon.
+          button.prepend('<span class="ui-current-item-icon ' + currentClasses + '"></span>')
+        },
+        select: function (event, ui) {
+          // Get current option's icon element.
+          var currentIcon = $('#' + this.id + '-button > .ui-current-item-icon');
 
-      // Set new classes for current option's icon element.
-      currentIcon.removeClass();
-      currentIcon.addClass(classes + ' ui-current-item-icon');
+          // Get selected option's classes.
+          var classes = ui.item.element.attr('data-class');
+
+          // Set new classes for current option's icon element.
+          currentIcon.removeClass();
+          currentIcon.addClass(classes + ' ui-current-item-icon');
+        },
+        change: function () {
+          $(this).trigger('change');
+        },
+        open: function() {
+          if ($().mCustomScrollbar) {
+            $(".ui-menu").mCustomScrollbar({
+              setHeight:200,
+              theme:"dark-3",
+              mouseWheel:{ preventDefault:true },
+              scrollButtons: { enable: true}
+            });
+          }
+        },
+        close: function() {
+          if ($().mCustomScrollbar) {
+            $(".ui-menu").mCustomScrollbar("destroy");
+          }
+        },
+        width: 200
+      });
     }
-  });
-})(jQuery);
+  }
+}(jQuery, Drupal, drupalSettings, this));
